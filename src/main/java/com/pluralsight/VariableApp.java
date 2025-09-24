a142cc800e744b904ca53635ad7017c0974f16ec
Qi Qing <learn.qiqing@gmail.com>
2025-09-24 14:35:12 -0400

    commented the if statement in havePets from line 20 to 23, not sure how to write it. added exercises step 2 answers.

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -8,6 +8,7 @@ public class VariableApp {
         // then use System.out.println() to display results
         // ex: System.out.println("The answer is " + answer);
         // REPEAT FOR NEXT EXERCISE
+        //step 1:
 
         String color = "purple";
         System.out.println("The answer for question 1, my favorite color is " + color);
@@ -23,6 +24,20 @@ public class VariableApp {
             System.out.println("The answer for question 4, whether I have pets is " + havePets);
         String niceMessage = "You look great.";
         System.out.println("The answer for question 5, a nice message is " + niceMessage);
+
+        //step 2:
+        int daysInWeek = 7;
+        double priceOfCoffee = 4.99;
+        char favoriteLetter = 'Q';
+        boolean isRaining = false;
+        System.out.println("There is " + daysInWeek + " days in a week.");
+        System.out.println("The price of coffee is $" + priceOfCoffee);
+        System.out.println("My favorite letter is " + favoriteLetter);
+        System.out.println("It is " + isRaining + "that it's raining.");
+
+
+
+
 
     }
 
