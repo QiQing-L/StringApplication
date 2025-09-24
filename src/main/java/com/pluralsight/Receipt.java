cec03266c94385cf1f4418543a9fcef1af209451
Qi Qing <learn.qiqing@gmail.com>
2025-09-24 17:50:43 -0400

    step 3, added file Receipt.java, set up main, declared variables, and set up println.

---
+++ src/main/java/com/pluralsight/Receipt.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Receipt {
+    public static void main(String[] args) {
+
+        String name;
+        double price;
+        int quantity;
+
+        System.out.println("You bought " + quantity + " " + name + "for $" + price);
+
+
+    }
+}
