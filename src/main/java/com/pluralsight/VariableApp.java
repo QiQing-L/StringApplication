1d468fa467c29761f6c0d98bada7329e2cd1d2b1
Qi Qing <learn.qiqing@gmail.com>
2025-09-24 14:39:15 -0400

    added periods to the print message, and change out middleinitial to none.

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -11,19 +11,19 @@ public class VariableApp {
         //step 1:
 
         String color = "purple";
-        System.out.println("The answer for question 1, my favorite color is " + color);
+        System.out.println("The answer for question 1, my favorite color is " + color + ".");
         int startYearOfThisClass = 2025;
-        System.out.println("The answer for question 2, the year I started this class is in " + startYearOfThisClass);
-        String middleInitial = " ";
-        System.out.println("The answer for question 3, my middle initial is " + middleInitial);
+        System.out.println("The answer for question 2, the year I started this class is in " + startYearOfThisClass + ".");
+        String middleInitial = "none";
+        System.out.println("The answer for question 3, my middle initial is " + middleInitial + ".");
         boolean havePets = false;
 //            if havePets = false:
 //               String pets = "no";
 //            else:
 //                String pets = "yes"
-            System.out.println("The answer for question 4, whether I have pets is " + havePets);
+            System.out.println("The answer for question 4, whether I have pets is " + havePets + ".");
         String niceMessage = "You look great.";
-        System.out.println("The answer for question 5, a nice message is " + niceMessage);
+        System.out.println("The answer for question 5, a nice message is " + niceMessage + ".");
 
         //step 2:
         int daysInWeek = 7;
@@ -34,6 +34,7 @@ public class VariableApp {
         System.out.println("The price of coffee is $" + priceOfCoffee);
         System.out.println("My favorite letter is " + favoriteLetter);
         System.out.println("It is " + isRaining + "that it's raining.");
+
 
 
 
