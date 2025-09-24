0ab4379fe3dcbe7b4e6812a3f8b48975d7f27a82
Qi Qing <learn.qiqing@gmail.com>
2025-09-24 14:04:58 -0400

    code question 1 step 1

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -2,6 +2,25 @@ package com.pluralsight;
 
 public class VariableApp {
     public static void main(String[] args) {
+        // Question 1:
+        // declare variables here
+        // then code solution
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);
+        // REPEAT FOR NEXT EXERCISE
+
+        String color = "red";
+        System.out.println("The answer for question 1 is " + color);
+        int startYearOfThisClass = 2025;
+        System.out.println("The answer for question 2 is " + startYearOfThisClass);
+        String middleInitial = " ";
+        System.out.println("The answer for question 3 is " + middleInitial);
+        boolean havePets = no;
+        System.out.println("The answer for question 4 is " + havePets);
+        String niceMessage = "You look great.";
+        System.out.println("The answer for question 5 is " + niceMessage);
 
     }
-}
+
+
+    }
